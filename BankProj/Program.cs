d829866a7cdcade406cfea2b646e3fed03def0cb
Lucas Bengtsson <lucas.bengtsson77@gmail.com>
2022-11-28 10:26:40 +0100

    first commit

---
+++ BankProj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
